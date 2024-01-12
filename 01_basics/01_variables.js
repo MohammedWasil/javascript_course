05a565613505068f377a7c539706fe318a3c143a
Mohammed Wasil <124422070+MohammedWasil@users.noreply.github.com>
2024-01-12 06:46:44 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 13453
+let accountEmail = "wasil@gmail.com"
+var accountPassword  = "23445"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "iurjhiljf.com"
+accountPassword = "54654435"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
