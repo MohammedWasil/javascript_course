01b120b0c169bbbb618feb2ded1bb4885e78719b
Mohammed Wasil <124422070+MohammedWasil@users.noreply.github.com>
2024-02-04 04:30:07 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "wasil"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined = >  
+
+// object
+ console.log(typeof "wasil")
+ console.log(typeof 34)
+ console.log(typeof null)
+ console.log(typeof undefined)
