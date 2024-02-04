9542b784df12af173eeb3515d2e944c8ba20d6d7
Mohammed Wasil <124422070+MohammedWasil@users.noreply.github.com>
2024-02-04 05:26:14 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,4 +18,15 @@ let someNumber = 44
 
 let stringNumber = String(someNumber)
 console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber) 
+
+// ----------------Operations --------------------------------
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "wasil"
+let str2 = " pasha"
+let str3 = str1 + str2
+console.log(str3)
