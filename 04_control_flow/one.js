088fe026801fce7e1064c95222d9a840ac11e37b
Mohammed Wasil <124422070+MohammedWasil@users.noreply.github.com>
2024-02-20 14:08:27 +0000

    control flow files

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if (temperature < 60){
+//     console.log("less than 60")
+// }else{
+//     console.log("temperature is greater than 60")
+// }
+
+// const score = 300
+// if (score > 200){
+//     let power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power: ${power}`)
+
+// const balance = 1000
+// // if(balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500){
+//     console.log("less than 500")
+// }else if (balance < 750){
+//     console.log("less than 750")
+// }else if (balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 3==4){
+    console.log("allow to buy course")
+}
+
+if (loggedInFromEmail || loggedInFromGoogle){
+    console.log("user logged in ")
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+const month = "april"
+switch (month){
+    case "January":
+        console.log("January")
+        break;
+    case "feburary":
+        console.log("feburary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april")
+        break;
+    default:
+        console.log("default case match")
+        break;
+}
