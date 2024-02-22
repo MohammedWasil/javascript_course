00e961ab62fc9a2c9daf657bb4d5dbea7f06f749
Mohammed Wasil <124422070+MohammedWasil@users.noreply.github.com>
2024-02-22 05:48:17 +0000

    loops discussion

---
+++ 05_iterations/four.js
@@ -0,0 +1,23 @@
+const myObect = {
+    js: 'javascript',
+    java: 'java',
+    rb: 'ruby',
+    swift: "swif by apple"
+}
+for (const key in myObect) {
+    // console.log(`${key} shortcut is for ${myObect[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming){
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for (const key in map){
+//     console.log(map[key])
+// }
